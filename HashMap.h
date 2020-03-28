54da2e2af0dc9a52f5e52e2e2781068133fb1c6c
vorobeevich <61282340+vorobeevich@users.noreply.github.com>
2020-03-28 14:27:25 +0300

    Update HashMap.h
    ID посылки - 31836073

--- HashMap.h
+++ HashMap.h
@@ -1,164 +1,172 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <iterator>
+#include <list>
+#include <stdexcept>
 
-template < class KeyType, class ValueType, class Hash = std :: hash < KeyType > >
-
-
-class HashMap{
-public:
-    using iterator = typename std :: list < std :: pair < const KeyType, ValueType > > :: iterator;
-    using const_iterator = typename std :: list < std :: pair < const KeyType, ValueType > > :: const_iterator;
-    HashMap(Hash hasher = Hash()) : hasher(hasher){
-        table_size_ = 10;
+template<class KeyType, class ValueType, class Hash = std::hash<KeyType> >
+class HashMap {
+ public:
+    using iterator = typename
+        std::list<std::pair<const KeyType, ValueType> >::iterator;
+    using const_iterator = typename
+        std::list<std::pair<const KeyType, ValueType> >::const_iterator;
+    HashMap(Hash hasher_ = Hash()) : hasher_(hasher_) {
+        table_size_ = start_size_;
         count_elements_ = 0;
         table.resize(table_size_);
     }
-    HashMap(const std :: initializer_list < std :: pair < const KeyType, ValueType > > &l, Hash hasher = Hash()) : HashMap(hasher){
-        for (auto it : l){
-            insert(it);
+    HashMap(const std::initializer_list<std::pair<const KeyType, ValueType> > &init,
+            Hash hasher_ = Hash()) : HashMap(hasher_) {
+        for (auto const &current_element : init) {
+            insert(current_element);
         }
     }
-    template < typename iter >
-    HashMap(iter l, iter r, Hash hasher = Hash()) : HashMap(hasher){
-        for (auto it = l; it != r; ++it){
-            insert(*it);
+    template <typename Inputit>
+    HashMap(Inputit first, Inputit last, Hash hasher_ = Hash()) : HashMap(hasher_) {
+        for (auto current_iter = first; current_iter != last; ++current_iter) {
+            insert(*current_iter);
         }
     }
-    int size() const{
+    size_t size() const {
         return count_elements_;
     }
-    bool empty() const{
+    bool empty() const {
         return count_elements_ == 0;
     }
-    void insert(std :: pair < const KeyType, ValueType > el);
-    Hash hash_function() const{
-        return hasher;
+    void insert(std::pair <const KeyType, ValueType> el);
+    Hash hash_function() const {
+        return hasher_;
     }
     void erase(const KeyType key);
-    iterator begin(){
+    iterator begin() {
         return all.begin();
     }
-    iterator end(){
+    iterator end() {
         return all.end();
     }
-    const_iterator begin() const{
+    const_iterator begin() const {
         return all.cbegin();
     }
-    const_iterator end() const{
+    const_iterator end() const {
         return all.cend();
     }
     void clear();
     iterator find(const KeyType key);
-
     const_iterator find(const KeyType key) const;
-
     ValueType &operator[](const KeyType key);
-
     const ValueType &at(const KeyType key) const;
+    HashMap &operator=(const HashMap &other);
 
-    HashMap &operator= (const HashMap &other);
-private:
-    int table_size_, count_elements_;
-    std :: list < std :: pair < const KeyType, ValueType> > all;
-    std :: vector < std :: list < typename std :: list < std :: pair < const KeyType, ValueType> > :: iterator > > table;
-    Hash hasher;
-    void rebuild(){
-        table_size_ *= 2;
-        table.clear();
-        table.resize(table_size_);
-        for (auto cur = all.begin(); cur != all.end(); cur++){
-            table[hasher((*cur).first) % table_size_].push_back(cur);
-        }
-    }
-
+ private:
+    size_t table_size_, count_elements_;
+    const size_t start_size_ = 10, change_size_ = 2;
+    std::list<std::pair<const KeyType, ValueType> > all;
+    std::vector<std::list<iterator> > table;
+    Hash hasher_;
+    void rebuild();
 };
 
 
-template < class KeyType, class ValueType, class Hash >
-typename std :: list < std :: pair < const KeyType, ValueType > > :: iterator HashMap < KeyType, ValueType, Hash > :: find(const KeyType key){
-    int cur = hasher(key) % table_size_;
-    for (auto tec : table[cur]){
-        if ((*tec).first == key){
-            return tec;
+template<class KeyType, class ValueType, class Hash>
+auto HashMap<KeyType, ValueType, Hash>:: find (const KeyType key) -> iterator {
+    size_t index = hasher_(key) % table_size_;
+    for (auto const &element : table[index]) {
+        if ((*element).first == key) {
+            return element;
         }
     }
     return all.end();
 }
 
-template < class KeyType, class ValueType, class Hash >
-typename std :: list < std :: pair < const KeyType, ValueType > > :: const_iterator HashMap < KeyType, ValueType, Hash > :: find(const KeyType key) const{
-    int cur = hasher(key) % table_size_;
-    for (auto tec : table[cur]){
-        if ((*tec).first == key){
-            return tec;
+template<class KeyType, class ValueType, class Hash>
+auto HashMap<KeyType, ValueType, Hash>:: find
+        (const KeyType key) const -> const_iterator {
+    size_t index = hasher_(key) % table_size_;
+    for (auto const &element : table[index]) {
+        if ((*element).first == key) {
+            return element;
         }
     }
     return all.cend();
 }
 
-template < class KeyType, class ValueType, class Hash >
-void HashMap < KeyType, ValueType, Hash > :: insert(std :: pair < const KeyType, ValueType > el){
-    int cur = hasher(el.first) % table_size_;
-    all.push_back(el);
-    iterator it = all.end();
-    it--;
-    table[cur].push_back(it);
+template<class KeyType, class ValueType, class Hash>
+void HashMap<KeyType, ValueType, Hash>::insert
+        (std::pair <const KeyType, ValueType> new_element) {
+    size_t index = hasher_(new_element.first) % table_size_;
+    all.push_back(new_element);
+    iterator new_iter = all.end();
+    new_iter--;
+    table[index].push_back(new_iter);
     count_elements_++;
-    if (count_elements_ == table_size_){
+    if (count_elements_ == table_size_) {
         rebuild();
     }
 }
 
-template < class KeyType, class ValueType, class Hash >
-void HashMap < KeyType, ValueType, Hash > :: erase(const KeyType key){
-    int cur = hasher(key) % table_size_;
-    for (auto it = table[cur].begin(); it != table[cur].end(); it++){
-        if ((*(*it)).first == key){
-            all.erase(*it);
-            table[cur].erase(it);
+template<class KeyType, class ValueType, class Hash>
+void HashMap<KeyType, ValueType, Hash>::erase(const KeyType key) {
+    size_t num = hasher_(key) % table_size_;
+    for (auto iter = table[num].begin(); iter != table[num].end(); iter++) {
+        if ((*(*iter)).first == key) {
+            all.erase(*iter);
+            table[num].erase(iter);
             count_elements_--;
             break;
         }
     }
 }
 
-template < class KeyType, class ValueType, class Hash >
-void HashMap < KeyType, ValueType, Hash > :: clear(){
+template<class KeyType, class ValueType, class Hash>
+void HashMap <KeyType, ValueType, Hash>::clear() {
     all.clear();
     table.clear();
     count_elements_ = 0;
-    table_size_ = 10;
+    table_size_ = start_size_;
     table.resize(table_size_);
 }
 
-template < class KeyType, class ValueType, class Hash >
-ValueType& HashMap < KeyType, ValueType, Hash > :: operator[](const KeyType key){
-    auto cur = find(key);
-    if (cur == all.end()){
+template<class KeyType, class ValueType, class Hash>
+ValueType& HashMap<KeyType, ValueType, Hash>::operator[]
+        (const KeyType key) {
+    auto result = find(key);
+    if (result == all.end()) {
         insert({key, {}});
         return (*find(key)).second;
     }
-    return (*cur).second;
+    return (*result).second;
 }
 
-template < class KeyType, class ValueType, class Hash >
-const ValueType& HashMap < KeyType, ValueType, Hash > :: at(const KeyType key) const{
+template<class KeyType, class ValueType, class Hash>
+const ValueType& HashMap<KeyType, ValueType, Hash>::at
+        (const KeyType key) const {
     const_iterator cur = find(key);
-    if (cur == all.cend()){
-        throw std :: out_of_range("");
+    if (cur == all.cend()) {
+        throw std::out_of_range("");
     }
     return (*cur).second;
 }
 
-template < class KeyType, class ValueType, class Hash >
-HashMap < KeyType, ValueType, Hash >& HashMap < KeyType, ValueType, Hash > :: operator= (const HashMap &other){
+template<class KeyType, class ValueType, class Hash>
+HashMap<KeyType, ValueType, Hash>& HashMap<KeyType, ValueType, Hash>::operator=
+        (const HashMap &other) {
     if (this == &other) {
         return *this;
     }
     clear();
-    for (auto it : other){
-        insert(it);
+    for (auto const &iter : other) {
+        insert(iter);
     }
     return *this;
 }
 
-
+template<class KeyType, class ValueType, class Hash>
+void HashMap<KeyType, ValueType, Hash>::rebuild() {
+    table_size_ *= change_size_;
+    table.clear();
+    table.resize(table_size_);
+    for (auto iter = all.begin(); iter != all.end(); ++iter) {
+        size_t index = hasher_((*iter).first) % table_size_;
+        table[index].push_back(iter);
+    }
+}
